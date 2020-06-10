0f6108cb63ca8ac6a1adce877e86e3d9a9cc3996
Przemek <p.grzelczyk@gmail.com>
2020-06-10 12:28:24 +0200

    loads all messages from server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const staticPosts = [{
-  name: 'anon1',
-  message: 'lorem ipsum dolor sit amet'
-},
-{
-  name: 'anon2',
-  message: 'lorem ipsum dolor sit amet'
-},
-{
-  name: 'anon3',
-  message: 'lorem ipsum dolor sit amet'
-},
-{
-  name: 'anon1',
-  message: 'lorem ipsum dolor sit amet'
-}]
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { allMessages: 'this is bullshit' };
-  }
-
-  // componentWillMount() {
-
-  //   this.callExpress()
-  //     .then(res =>
-  //       this.setState({ allMessages: res.dataset })
-  //     )
-  //     .catch(err => console.error(err));
-  // }
-
-  componentWillMount() {
-    this.setState({ allMessages: staticPosts })
-    this.setState({ comment: 'Hello' });
+    this.state = {
+      isLoading: true
+    }
 
   }
   componentDidMount() {
-    this.setAnotherState()
-    console.log("state after mount: ", this.state)
+    this.fetchAllMessages()
   };
 
-  callExpress = async () => {
+
+  fetchAllMessages = async () => {
     const response = await fetch('/api/messages');
     const body = await response.json();
 
     if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  }
-
-  setAnotherState() {
-    this.setState({ newMessages: 'Iono habla espagnol' });
+    this.setState({ allMessages: body.dataset, isLoading: false })
   }
 
   render() {
     return (
       <div className="App">
 
-
+        <p>Here be my messages</p>
+        <p>if I had any</p>
         <div >
-          {this.state.allMessages.map((post, index) =>
-            <article key={index}>
-              <h3>
-                {post.name}
-              </h3>
-              <p>
-                {post.message}
-              </p>
-            </article>
-          )}
+          {!this.state.isLoading &&
+            this.state.allMessages.map((post, index) =>
+              <article key={index}>
+                <h3>
+                  {post.name}
+                </h3>
+                <p>
+                  {post.message}
+                </p>
+              </article>
+            )}
         </div>
 
 
