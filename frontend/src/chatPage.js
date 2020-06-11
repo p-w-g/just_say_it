0d38991e1c6e72bf6b7948402e0d28d3eabe1f4e
Przemek <p.grzelczyk@gmail.com>
2020-06-11 13:48:42 +0200

    adds all messages page back

---
+++ frontend/src/chatPage.js
@@ -0,0 +1,64 @@
+class ChatPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+  componentDidMount() {
+    this.fetchAllMessages()
+  };
+
+  fetchAllMessages = async () => {
+    const response = await fetch('/api/messages');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+    this.setState({ allMessages: body.dataset, isLoading: false })
+  }
+  render() {
+    return (
+      <div >
+        {!this.state.isLoading &&
+          this.state.allMessages.map((post, index) =>
+            <article key={index}>
+              <h3>
+                {post.name}
+              </h3>
+              <p>
+                {post.message}
+              </p>
+            </article>
+          )}
+      </div>
+    );
+  }
+}
+// class EssayForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { value: 'Please write an essay about your favorite DOM element.' };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(event) { this.setState({ value: event.target.value }); }
+//   handleSubmit(event) {
+//     alert('An essay was submitted: ' + this.state.value);
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Essay:
+//           <textarea value={this.state.value} onChange={this.handleChange} />
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+
+export default ChatPage;
