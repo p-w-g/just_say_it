78eb9d5f0ecb141136d3c9f3a43f6f82533009f7
Przemek <p.grzelczyk@gmail.com>
2020-06-29 14:45:56 +0200

    splits components

--- frontend/src/components/ChatPage.js
+++ frontend/src/components/ChatPage.js
@@ -3,6 +3,7 @@ import { useStore } from '../Store'
 import io from 'socket.io-client'
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
+import PostForm from './PostForm'
 
 const [store, setStore] = useStore();
 const socket = io()
@@ -124,57 +125,6 @@ class ChatPage extends React.Component {
           </Alert>
         </Snackbar>
       </div>
-    );
-  }
-}
-
-class PostForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) { this.setState({ value: event.target.value }); }
-  handleSubmit = async (event) => {
-    event.preventDefault();
-
-    await this.postThePost()
-
-    document.querySelector('#chat-input').value = '';
-    // await this.postCheck(this.state.value) ? this.postThePost() : console.error("Something went wrong.")
-  }
-
-  postCheck = content => {
-    if (!content) return alert("Missing content")
-    if (content === '') return alert("Missing content")
-    // if (content.match(/[^A-Za-z0-9,.& ']+/g)) return alert("Only alphanumeric characters and: , ' &")
-
-    return true
-  }
-
-  initSocket = () => {
-    socket.on('connect', () => {
-    })
-  }
-
-  componentDidMount() {
-    this.initSocket()
-  };
-
-  postThePost = async content => {
-    socket.emit('chat message', ({ name: store().name, message: this.state.value }))
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} class="">
-        <label>
-          <input class="form-control form-control-sm" onChange={this.handleChange} id="chat-input" />
-        </label>
-        <input type="submit" value="Send" class="btn btn-outline-info btn-sm btn-fix" />
-      </form>
     );
   }
 }
