fc06a079a70e117a5db6aa6ac8587059c45b8c59
Przemek <p.grzelczyk@gmail.com>
2020-06-30 14:44:12 +0200

    refactor loginhandler component to functional component, remove redundant submit click handlers

--- frontend/src/components/LoginHandler.js
+++ frontend/src/components/LoginHandler.js
@@ -2,37 +2,18 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import NameForm from './NameForm'
 import ChatPage from './ChatPage'
+import { useStore } from 'react-redux'
 
-class LoginHandler extends Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = { isLoggedIn: false };
-  }
 
-  handleLoginClick() {
-    this.setState({ isLoggedIn: true });
-  }
-
-  handleLogoutClick() {
-    this.setState({ isLoggedIn: false });
-  }
-
-  render() {
-    return (
-      <div class="container-fluid d-flex justify-content-center align-items-center">
-        {
-          this.state.isLoggedIn ? <ChatPage forceLogout={this.handleLogoutClick.bind(this)} />
-            : <NameForm forceLogin={this.handleLoginClick.bind(this)} />
-        }
-
-      </div >
-    );
-  }
+const LoginHandler = () => {
+  const store = useStore()
+  console.log(store.getState())
+  return <div class="container-fluid d-flex justify-content-center align-items-center">
+    {
+      store.getState().isLoggedIn ? <ChatPage />
+        : <NameForm />
+    }
+  </div>
 }
-const mapStateToProps = state => ({
-  isLoggedIn: state.isLoggedIn
-})
 
 export default connect()(LoginHandler);
